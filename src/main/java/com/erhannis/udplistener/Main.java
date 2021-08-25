f2175708b7b512107e05fc54de707eb571ea3cc1
erhannis <eyeillus@gmail.com>
2021-08-25 01:51:26 -0400

    Increased buffer size to max size of udp packet, according to google

--- src/main/java/com/erhannis/udplistener/Main.java
+++ src/main/java/com/erhannis/udplistener/Main.java
@@ -26,7 +26,7 @@ public class Main {
       // Create a buffer to read datagrams into. If a
       // packet is larger than this buffer, the
       // excess will simply be discarded!
-      byte[] buffer = new byte[2048];
+      byte[] buffer = new byte[65507];
 
       // Create a packet to receive data into the buffer
       DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
@@ -38,8 +38,7 @@ public class Main {
 
         // Convert the contents to a string, and display them
         String msg = new String(buffer, 0, packet.getLength());
-        System.out.println(packet.getAddress().getHostName() + ": "
-                + msg);
+        System.out.println(packet.getAddress().getHostName() + ": " + msg);
 
         // Reset the length of the packet before reusing it.
         packet.setLength(buffer.length);
